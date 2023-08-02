9d285aebfa49b9ac6c668a0eeb58a4d65484c6a3
Cristhian Vera <cristianmario092190@gmail.com>
2023-08-02 10:55:43 -0500

    feat: add update category and validation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -32,7 +32,7 @@ class CategoriesController < ApplicationController
   # PATCH/PUT /categories/1
   def update
     if @category.update(category_params)
-      redirect_to @category, notice: "Category was successfully updated."
+      redirect_to root_path, notice: "Category was successfully updated."
     else
       render :edit, status: :unprocessable_entity
     end
